06ce297ae66da94980cf0449fdad84c677a32a1d
Rezvandoost1401 <110698867+Rezvandoost1401@users.noreply.github.com>
2022-09-16 14:44:14 +0430

    Update settings.py

--- Digi_Pro/settings.py
+++ Digi_Pro/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4kwzvz+k2g1dt8dn18r+d_=c(2a6oswy!9rv%)j$(+xh$yuwak'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
